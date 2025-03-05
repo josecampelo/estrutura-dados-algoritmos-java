5d79529d1fe7a83827217b0e0d8886a61dd937b9
Campelo <josecampelojr@gmail.com>
2025-03-05 12:23:37 -0300

    Aula 06: Criacao do segundo metodo de busca, esse para verificar se o elemento existe no array ou nao

--- src/br/com/josecampelo/estruturadados/vetor/Vetor.java
+++ src/br/com/josecampelo/estruturadados/vetor/Vetor.java
@@ -57,6 +57,18 @@ public class Vetor {
         return this.elementos[posicao];
     }
 
+    public int busca(String elemento) {
+
+        for (int i = 0; i < this.tamanho; i++) {
+
+            if (this.elementos[i].equals(elemento)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
     public int getTamanho() {
         return this.tamanho;
     }
