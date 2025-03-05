0f9087d1cb4d41ad87de38e83b4cb375f8a77965
Campelo <josecampelojr@gmail.com>
2025-03-05 10:29:33 -0300

    Aula 04: Adiciona o metodo get para tamanho e sobrescreve o metodo toString de Vetor para retornar somente os elementos inseridos

--- src/br/com/josecampelo/estruturadados/vetor/Vetor.java
+++ src/br/com/josecampelo/estruturadados/vetor/Vetor.java
@@ -1,5 +1,7 @@
 package br.com.josecampelo.estruturadados.vetor;
 
+import java.util.Arrays;
+
 public class Vetor {
 
     private String[] elementos;
@@ -44,5 +46,29 @@ public class Vetor {
         }
 
         return false;
+    }
+
+    public int getTamanho(){
+        return this.tamanho;
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder s = new StringBuilder();
+        s.append("[");
+
+        for (int i = 0; i < this.tamanho-1; i++) {
+            s.append(this.elementos[i]);
+            s.append(", ");
+        }
+
+        if (this.tamanho > 0) {
+            s.append(this.elementos[this.tamanho-1]);
+        }
+
+        s.append("]");
+
+        return s.toString();
     }
 }
---
+++ src/br/com/josecampelo/estruturadados/vetor/teste/Aula04.java
@@ -0,0 +1,19 @@
+package br.com.josecampelo.estruturadados.vetor.teste;
+
+import br.com.josecampelo.estruturadados.vetor.Vetor;
+
+public class Aula04 {
+
+    public static void main(String[] args) {
+        Vetor vetor = new Vetor(10);
+
+        vetor.adiciona("elemento 1");
+        vetor.adiciona("elemento 2");
+        vetor.adiciona("elemento 3");
+
+        System.out.println(vetor.getTamanho());
+
+        System.out.println(vetor);
+    }
+
+}
