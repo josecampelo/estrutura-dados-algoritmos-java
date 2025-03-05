fbcac6ac2d2c71c7ee0ea3e3444916759f650804
Campelo <josecampelojr@gmail.com>
2025-03-05 09:53:57 -0300

    Aula 3: Criando o método de adicionar um elemento dentro  vetor

---
+++ src/br/com/josecampelo/estruturadados/vetor/teste/Aula03.java
@@ -0,0 +1,14 @@
+package br.com.josecampelo.estruturadados.vetor.teste;
+
+import br.com.josecampelo.estruturadados.vetor.Vetor;
+
+public class Aula03 {
+    public static void main(String[] args) {
+
+        Vetor vetor = new Vetor(2);
+
+        vetor.adiciona("elemento 1");
+        vetor.adiciona("elemento 2");
+        vetor.adiciona("elemento 3");
+    }
+}
