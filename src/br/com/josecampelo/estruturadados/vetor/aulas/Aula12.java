a0fd7936a38ed91fbec74d0a1443e031bb2036e8
Campelo <josecampelojr@gmail.com>
2025-03-06 16:29:25 -0300

    Aula 12: Conhecendo API Java: ArrayList

---
+++ src/br/com/josecampelo/estruturadados/vetor/aulas/Aula12.java
@@ -0,0 +1,24 @@
+package br.com.josecampelo.estruturadados.vetor.aulas;
+
+import java.util.ArrayList;
+
+public class Aula12 {
+    public static void main(String[] args) {
+        ArrayList<String> nomes = new ArrayList<>();
+
+        nomes.add("Andre");
+        nomes.add("Leandro");
+        nomes.add("Junior");
+        nomes.add("David");
+        nomes.add("Allan");
+        nomes.add("Guilherme");
+
+        System.out.println("Elementos da lista:");
+        System.out.println(nomes);
+        System.out.println("Tamanho da lista: " + nomes.size());
+        System.out.println("Index que o elemento pertence " + nomes.indexOf("Junior"));
+        System.out.println("Adiciona elemento no topo da lista");
+        nomes.add(0, "Eduardo");
+        System.out.println(nomes);
+    }
+}
