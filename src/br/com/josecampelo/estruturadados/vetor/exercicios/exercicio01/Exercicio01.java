ee2bfc964e228a970f47424f158e7ed95438083d
Campelo <josecampelojr@gmail.com>
2025-03-07 11:39:40 -0300

    Exercicio 01: Criando o metodo contem() para o VetorExercicio

---
+++ src/br/com/josecampelo/estruturadados/vetor/exercicios/exercicio01/Exercicio01.java
@@ -0,0 +1,29 @@
+package br.com.josecampelo.estruturadados.vetor.exercicios.exercicio01;
+
+import br.com.josecampelo.estruturadados.vetor.exercicios.VetorExercicio;
+
+public class Exercicio01 {
+    public static void main(String[] args) {
+        /*
+            # Exercicio 01:
+            Melhore a classe Vetor e implemente o metodo contem, semelhante ao metodo da classe ArrayList
+        */
+
+        VetorExercicio<String> vetorExercicio = new VetorExercicio<>(5);
+        vetorExercicio.adiciona("Andre");
+        vetorExercicio.adiciona("Leandro");
+        vetorExercicio.adiciona("Junior");
+        vetorExercicio.adiciona("David");
+        vetorExercicio.adiciona("Allan");
+
+        String elemento = "Junior";
+
+        System.out.println("O Vetor possui o elemento: " + elemento + "?");
+
+        if (vetorExercicio.contem(elemento)) {
+            System.out.println("Sim, o vetor contém o elemento " + elemento);
+        } else {
+            System.out.println("Não, o vetor não contém o elemento " + elemento);
+        }
+    }
+}
