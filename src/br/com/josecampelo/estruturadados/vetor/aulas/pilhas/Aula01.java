f897be7422ce939548598d1b928c3b7e3f12da9f
Campelo <josecampelojr@gmail.com>
2025-03-10 07:46:41 -0300

    Pilhas aula 01: Criação da super classe EstruturaEstatica e da classe Pilha

---
+++ src/br/com/josecampelo/estruturadados/vetor/aulas/pilhas/Aula01.java
@@ -0,0 +1,10 @@
+package br.com.josecampelo.estruturadados.vetor.aulas.pilhas;
+
+public class Aula01 {
+    public static void main(String[] args) {
+        Pilha<Integer> pilha = new Pilha<>();
+
+        System.out.println(pilha);
+        System.out.println(pilha.getTamanho());
+    }
+}
---
+++ src/br/com/josecampelo/estruturadados/vetor/aulas/pilhas/Pilha.java
@@ -0,0 +1,16 @@
+package br.com.josecampelo.estruturadados.vetor.aulas.pilhas;
+
+import br.com.josecampelo.estruturadados.vetor.aulas.base.EstruturaEstatica;
+
+public class Pilha<T> extends EstruturaEstatica<T> {
+
+    public Pilha() {
+        super();
+    }
+
+    public Pilha(int capacidade) {
+        super(capacidade);
+    }
+
+
+}
