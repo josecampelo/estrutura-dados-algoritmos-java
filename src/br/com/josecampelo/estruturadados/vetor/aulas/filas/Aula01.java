5fb410618a00c90e06d6095b07f3386720ea6cb5
Campelo <josecampelojr@gmail.com>
2025-03-16 20:04:13 -0300

    Fila aula 01: Criação da classe Fila

---
+++ src/br/com/josecampelo/estruturadados/vetor/aulas/filas/Aula01.java
@@ -0,0 +1,10 @@
+package br.com.josecampelo.estruturadados.vetor.aulas.filas;
+
+public class Aula01 {
+    public static void main(String[] args) {
+        Fila<Integer> fila = new Fila<>();
+
+        System.out.println(fila.estaVazia());
+        System.out.println(fila.getTamanho());
+    }
+}
---
+++ src/br/com/josecampelo/estruturadados/vetor/aulas/filas/Fila.java
@@ -0,0 +1,28 @@
+package br.com.josecampelo.estruturadados.vetor.aulas.filas;
+
+import br.com.josecampelo.estruturadados.vetor.aulas.base.EstruturaEstatica;
+
+public class Fila<T> extends EstruturaEstatica<T> {
+
+    public Fila() {
+        super();
+    }
+
+    public Fila(int capacidade) {
+        super(capacidade);
+    }
+
+    /*
+    public void enfileira(T elemento) {
+
+    }
+
+    public T espia() {
+
+    }
+
+    public T desenfileira() {
+
+    }
+     */
+}
