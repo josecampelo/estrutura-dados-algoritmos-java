93f5e0a6a0e06face4882d1022b3abb3021d987e
Campelo <josecampelojr@gmail.com>
2025-03-17 09:50:53 -0300

    Exercício de fila 01: Criação de um programa que adiciona documentos a uma fila para impressão

---
+++ src/br/com/josecampelo/estruturadados/vetor/exercicios/filas/Documento.java
@@ -0,0 +1,25 @@
+package br.com.josecampelo.estruturadados.vetor.exercicios.filas;
+
+public class Documento {
+
+    private String nome;
+    private int quantidadeFolhas;
+
+    public Documento(String nome, int quantidadeFolhas) {
+        this.nome = nome;
+        this.quantidadeFolhas = quantidadeFolhas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getQuantidadeFolhas() {
+        return quantidadeFolhas;
+    }
+
+    @Override
+    public String toString() {
+        return nome + " com " + quantidadeFolhas + " folhas";
+    }
+}
---
+++ src/br/com/josecampelo/estruturadados/vetor/exercicios/filas/Exercicio01.java
@@ -0,0 +1,39 @@
+package br.com.josecampelo.estruturadados.vetor.exercicios.filas;
+
+import br.com.josecampelo.estruturadados.vetor.aulas.filas.Fila;
+
+public class Exercicio01 {
+    public static void main(String[] args) {
+        // Exercício 1 - http://goo.gl/fFQCDD
+
+        Fila<Documento> fila = new Fila<>();
+
+        fila.enfileira(new Documento("Contas", 25));
+        fila.enfileira(new Documento("Anotações", 70));
+        fila.enfileira(new Documento("Material de estudo", 200));
+        fila.enfileira(new Documento("Desenhos", 36));
+        fila.enfileira(new Documento("Resumo do curso", 93));
+        fila.enfileira(new Documento("Calculo matemático", 2));
+        fila.enfileira(new Documento("Contas antigas", 19));
+
+        System.out.println("Iniciando programa de imprimir documentos...");
+        System.out.println();
+
+        while (!fila.estaVazia()) {
+            System.out.println("Imprimindo documento " + proximoDucimento(fila) + "...");
+            System.out.println("Documento " + imprimeDocumento(fila) + " imprimido com sucesso!");
+            System.out.println("Iniciando a próxima impressão...");
+            System.out.println();
+        }
+
+        System.out.println("Fila de impressão vazia, encerrando programa...");
+    }
+
+    private static Documento proximoDucimento(Fila<Documento> fila) {
+        return fila.espia();
+    }
+
+    private static Documento imprimeDocumento(Fila<Documento> fila) {
+        return fila.desenfileira();
+    }
+}
